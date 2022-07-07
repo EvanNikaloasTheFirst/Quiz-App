4bf9769494eab7be79d733ee4a96c9eb2d70cefd
EvanNikaloasTheFirst <evangelosniko@outlook.com>
2022-07-07 08:51:25 +0100

    Can itterate over questions via button

--- quizApp.js
+++ quizApp.js
@@ -1,37 +1,92 @@
-let scoreBoard = document.getElementById('score');
+
 let UserScore = 0;
 
-var counter = 0;
+let availableQuestions = [];
+// Contains all questions
+let allQuestions = [];
+let correctAnswers = []
+let questionsAsked = 0;
+// starts on minus 1 so question 0 is displayed
+var counter = -1;
+var questions = document.getElementById("questions");
+
 // fetch retieves API URL 
-// .then works with the JSON after its been retrieved
-fetch("https://opentdb.com/api.php?amount=10&category=32&type=boolean").then(function(response){
+let apiKey = "https://opentdb.com/api.php?amount=10&category=32&type=boolean";
+
+const choices = document.getElementsByClassName("options")
+console.log(choices)
+
+fetch(apiKey).then(response =>{
     // converts the body of the response (JSON) - > then return new promise
     return response.json();
-    startQuiz()
+    
+//.then = when the data has loaded
+}).then(QuizQuestions => {
 
-}).then(function(data){
+// Maps all the indiviual questions within the array to become singular objects
+QuizQuestions.results.map(QuizQuestion => {
+    const formattedQuestion = {
+        question: QuizQuestion.question
+    };
 
-    const {question} = data.results[0];
-    const {correct_answer} = data.results[0];
-     
-    console.log("question:" + question)
-    console.log("correct_answer: " + correct_answer)
+    
 
+// itterates over all questions and pushes it to a new array
+    for (const [key,value] of Object.entries(formattedQuestion)){
+        // console.log(`${key}: ${value}`)
 
-
-    for (var i = 0; i < question.length; i++){
-        for (var j = 0; i < correct_answer.length;j++){
-    var questions = document.getElementById('questions').innerHTML = question[i];
-    if (document.getElementById('true').clicked == true && correct_answer[j] == 'True'){
-        
-
+        allQuestions.push(value)
     }
-}
+    // console.log(allQuestions)
 
-}
+    // correct answer for each question
+    const correctAnswer = [QuizQuestion.correct_answer]
+    // pushed all of the correct answers into an array
+    for (const [key,value] of Object.entries(correctAnswer)){
+        // console.log(`${key}: ${value}`)
+
+        correctAnswers.push(value)
+    }
+    // console.log(correctAnswers)
+
+     startGame ()
+})
 })
 .catch(function(error){
-    console.log("Something went wrong retrieveing the JSON")
+    console.log("Something went wrong retrieving the JSON")
 
 })
+
+
+function startGame (formattedQuestion){
+
+var arrayLength = allQuestions.length;
+// console.log (arrayLength)
+   
+// once button is clicked
+    document.getElementById('startBtn').onclick = function (){
+    
+        console.log('clicked')
+        // console.log(allQuestions)
+        if (counter < allQuestions.length){
+            questions.innerHTML = arrayLength[counter];
+            counter = counter + 1;
+
+            console.log(questions.innerHTML = allQuestions[counter])
+        }
+        else{
+            console.log('Quiz finished')
+        }
+
+}
+
+}
+
+function myLoadFunction(){
+    startBtn.addEventListener('click',startGame);
+    
+}
+
+document.addEventListener('DOMContentLoaded', myLoadFunction)
+
 
